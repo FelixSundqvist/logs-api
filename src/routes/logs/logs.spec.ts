6898415b38cd62ecefe2f5a88e391ec672f464fc
Felix Sundqvist <48220704+FelixSundqvist@users.noreply.github.com>
2025-03-28 12:19:24 +0200

    chore: better tests

--- src/routes/logs/logs.spec.ts
+++ src/routes/logs/logs.spec.ts
@@ -1,11 +1,11 @@
 import { test } from "@japa/runner";
-import { createLog, getAllLogs } from "./logs.service.js";
+import { createLog, deleteLog, getAllLogs } from "./logs.service.js";
 import { Log } from "./logs.types.js";
 
 test.group("Logs", () => {
   test("getAllLogs()", async ({ expectTypeOf }) => {
     const logs = await getAllLogs();
-    expectTypeOf(logs).toBeArray();
+    expectTypeOf(logs).toEqualTypeOf<Log[]>();
   });
   test("createLog()", async ({ expectTypeOf }) => {
     const response = await createLog(
@@ -14,6 +14,7 @@ test.group("Logs", () => {
       }),
     );
     expectTypeOf(response).toEqualTypeOf<Log>();
+    await deleteLog(response.id);
   });
 });
 
@@ -22,13 +23,18 @@ test.group("/api/logs", () => {
     const response = await client.get("/api/logs");
     expect(response.status()).toBe(200);
   });
-  test("/POST /api/logs", async ({ expect, client }) => {
+  test("/POST /api/logs", async ({ expect, expectTypeOf, client }) => {
     const request = client.post("/api/logs").json({
       json: {
         test: "foo",
       },
     });
+
     const response = await request;
     expect(response.status()).toBe(201);
+
+    const body = response.body() as Log;
+    expectTypeOf(body).toEqualTypeOf<Log>();
+    await deleteLog(body.id);
   });
 });
